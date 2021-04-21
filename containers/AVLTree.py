99ba3e8ca8e23ac1a966d42585697539575f6210
sepstein22 <sepstein22@cmc.edu>
2021-04-20 18:08:55 -0700

    3

--- containers/AVLTree.py
+++ containers/AVLTree.py
@@ -1,5 +1,6 @@
 '''
 This file implements the AVL Tree data structure.
+but there are fewer of them.
 '''
 
 from containers.BinaryTree import BinaryTree, Node
@@ -39,76 +40,68 @@ class AVLTree(BST):
         return BinaryTree._height(node.left) - BinaryTree._height(node.right)
 
     def is_avl_satisfied(self):
-        '''
-        Returns True if the avl tree satisfies that all nodes
-        have a balance factor in [-1,0,1].
-        '''
         return AVLTree._is_avl_satisfied(self.root)
 
     @staticmethod
     def _is_avl_satisfied(node):
         '''
+        FIXME:
         Implement this function.
         '''
         if node is None:
             return True
-        left = AVLTree._is_avl_satisfied(node.left)
-        right = AVLTree._is_avl_satisfied(node.right)
-        return AVLTree._balanced_factor(node) in [-1, 0, 1] and left and right
+        x = AVLTree._is_avl_satisfied(node.left)
+        y = AVLTree._is_avl_satisfied(node.right)
+        return AVLTree._balance_factor(node) in [-1, 0, 1] and x and y
 
     @staticmethod
     def _left_rotate(node):
         '''
         FIXME:
         Implement this function.
-
         The lecture videos provide a high-level overview of tree rotations,
         and the textbook provides full python code.
         '''
-        if node.right is None or node is None:
+        if node is None or node.right is None:
             return node
 
-        newnode = Node(node.right.value)
-        newnode.right = node.right.right
+        noda = Node(node.right.value)
+        noda.right = node.right.right
 
-        newleft = Node(node.value)
-        newleft.left = node.left
-        newleft.right = node.right.left
+        leftnode = Node(node.value)
+        leftnode.left = node.left
+        leftnode.right = node.right.left
 
-        newnode.left = newleft
+        noda.left = leftnode
 
-        return newnode
+        return noda
 
     @staticmethod
     def _right_rotate(node):
         '''
         FIXME:
         Implement this function.
+        The lecture videos provide a high-level overview of tree rotations,
+        and the textbook provides full python code.
         '''
-        if node.left is None or node is None:
+        if node is None or node.left is None:
             return node
 
-        newnode = Node(node.left.value)
-        newnode.left = node.left.left
+        noda = Node(node.left.value)
+        noda.left = node.left.left
 
-        newright = Node(node.value)
-        newright.right = node.right
-        newnode.left = node.left.right
+        node_right = Node(node.value)
+        node_right.right = node.right
+        node_right.left = node.left.right
 
-        newnode.right = newright
+        noda.right = node_right
 
-        return newnode
+        return noda
 
     def insert(self, value):
         '''
         FIXME:
         Implement this function.
-
-        HINT:
-        It is okay to add @staticmethod helper functions for this code.
-        The code should look very similar to the code for your insert
-        function for the BST,
-        but it will also call the left and right rebalancing functions.
         '''
         if not self.root:
             self.root = Node(value)
@@ -140,15 +133,15 @@ class AVLTree(BST):
             else:
                 return AVLTree._insert(value, node.right)
 
-    def rebalanced(self, start):
-        if start is None:
+    def rebalance(self, x):
+        if x is None:
             return
-        if self._balance_factor(start) in [-2, 2]:
-            start = self._rebalance(start)
+        if self._balance_factor(x) in [-2, 2]:
+            x = self._rebalance(x)
         else:
-            start.left = self.rebalance(start.left)
-            start.right = self.rebalance(start.right)
-        return start
+            x.left = self.rebalance(x.left)
+            x.right = self.rebalance(x.right)
+        return x
 
     @staticmethod
     def _rebalance(node):
