a81bba98abfbbca69d3acd334a20539a1516180d
sepstein22 <sepstein22@cmc.edu>
2021-05-02 11:16:26 -0700

    3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
-    name="cmc_csci046_sepstein22_containers",
+    name="cmc_csci046_sepstein22_container",
     version="1.0.0",
     description="Tree Week 13 homework, publishing repo",
     long_description=README,
